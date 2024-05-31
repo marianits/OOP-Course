f1b508078229c029b237f27b1246a5e55e95e70a
Mariana Arnez <mariana.arnez99@gmail.com>
2024-05-30 21:32:09 -0400

    First commit

---
+++ Task2/Month.cs
@@ -0,0 +1,45 @@
+﻿namespace Curso_OOP_Tarea_2
+{
+    public struct Month
+    {
+        public int MonthD;
+        public const int MinValue = 1;
+        public const int MaxValue = 12;
+
+        public Month()
+        {
+            this.MonthD = MinValue;
+        }
+        public Month(int month)
+        {
+            if (month < MinValue || month > MaxValue) throw new ArgumentOutOfRangeException(nameof(month));
+            this.MonthD = month;
+        }
+
+        public static implicit operator Month(int value)
+        {
+            return new Month(value);
+        }
+
+        public override string ToString()
+        {
+            return ((Months)this.MonthD).ToString();
+        }
+    }
+
+    public enum Months
+    {
+        January = 1,
+        February = 2,
+        March = 3,
+        April = 4,
+        May = 5,
+        June = 6,
+        July = 7,
+        August = 8,
+        September = 9,
+        October = 10,
+        November = 11,
+        December = 12
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿using Curso_OOP_Tarea_2;
+
+Month month1 = new Month();
+
+Month month2 = 7;
+Month month3 = Month.MaxValue;
+
+// Month month4 = 0; OUT OF RANGE
+// Month month5 = 13; OUT OF RANGE
+
+Console.WriteLine(month1);
+Console.WriteLine(month2);
+Console.WriteLine(month3);
